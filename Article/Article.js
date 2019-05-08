a3693d78abf50a152bfef8bcee956dc3214be41f
teej387 <teej387@yahoo.com>
2019-05-07 20:39:25 -0500

    imported Article.less into Index.less

--- Article/Article.js
+++ Article/Article.js
@@ -3,11 +3,11 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     
   }
@@ -26,4 +26,7 @@ class Article {
 
 */
 
-let articles;
+let articles = document.getElementsByClassName('article');
+articles.array.forEach(article => {
+  article = new Article;
+});
